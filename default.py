2d6e202bf1b46c3c34f8864afa8fb103af134446
Michael Simmonds <xbmc@filmon.com>
2013-03-27 16:37:55 +0000

    pushing GA

--- default.py
+++ default.py
@@ -4,6 +4,8 @@ import urllib,urllib2,re,sys,socket,os,md5,datetime,xbmcplugin,xbmcgui, xbmcaddo
 sys.path.insert(0, xbmc.translatePath(os.path.join('special://home/addons/plugin.video.itv', 'lib')))
 import utils, httplib2, socks, httplib, logging, time
 import urllib2
+import datetime
+import time
 ADDON = xbmcaddon.Addon(id='plugin.video.itv')
 icon = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.itv', 'icon.png'))
 foricon = xbmc.translatePath(os.path.join('special://home/addons/plugin.video.itv', ''))
@@ -12,6 +14,14 @@ datapath = xbmc.translatePath(ADDON.getAddonInfo('profile'))
 favorites = os.path.join(datapath, 'favorites')
 if os.path.exists(favorites)==True:
     FAV = open(favorites).read()
+    
+if ADDON.getSetting('visitor_ga')=='':
+    from random import randint
+    ADDON.setSetting('visitor_ga',str(randint(0, 0x7fffffff)))
+
+VERSION = "2.0.5"
+PATH = "ITV"            
+UATRACK="UA-35537758-1"   
 
 # setup cache dir
 __scriptname__  = 'ITV'
@@ -173,6 +183,7 @@ def PLAY_STREAM(name,url):
     pl.clear()
     pl.add(url, liz)
     xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(pl)
+    GA('Live',name.replace(' Live',''))
 
 def STREAMS():
         streams=[]
@@ -270,6 +281,7 @@ def getFavorites():
 def addFavorite(name,url,iconimage):
         import json
         favList = []
+        GA('Favourite ADD',name.split(' -')[0])
         if os.path.exists(favorites)==False:
             print 'Making Favorites File'
             favList.append((name.split(' -')[0],url,iconimage))
@@ -295,6 +307,7 @@ def addFavorite(name,url,iconimage):
 def rmFavorite(name):
         import json
         print 'Remove Favorite'
+        GA('Favourite Remove',name)
         with open(favorites) as f:
             a = f.read()
         data = json.loads(a)
@@ -547,8 +560,8 @@ def VIDEO(url):
                     there_are_subtitles=1
         
             for index in range(len(mediafile)):
-                print ("MEDIA ENTRY %d %s"),index, mediafile[index]
-        
+                #print ("MEDIA ENTRY %d %s"),index, mediafile[index]
+                print "MEDIA ENTRY"
         
             quality = int(__settings__.getSetting('video_stream'))
             selected_stream = 4
@@ -596,6 +609,10 @@ def VIDEO(url):
             play.add(url,listitem)
             player.play(play)
             pDialog.close()
+            try:
+                GA('Player',name.split(':')[0])
+            except:
+                GA('Player',name)
             if (there_are_subtitles == 1):
                 player.setSubtitles(subtitles_file)
         except:
@@ -664,7 +681,169 @@ def get_url(url):
             raise
     
     return data
+    
+    
+def parseDate(dateString):
+    try:
+        return datetime.datetime.fromtimestamp(time.mktime(time.strptime(dateString.encode('utf-8', 'replace'), "%Y-%m-%d %H:%M:%S")))
+    except:
+        return datetime.datetime.today() - datetime.timedelta(days = 1) #force update
 
+
+def checkGA():
+
+    secsInHour = 60 * 60
+    threshold  = 2 * secsInHour
+
+    now   = datetime.datetime.today()
+    prev  = parseDate(ADDON.getSetting('ga_time'))
+    delta = now - prev
+    nDays = delta.days
+    nSecs = delta.seconds
+
+    doUpdate = (nDays > 0) or (nSecs > threshold)
+    if not doUpdate:
+        return
+
+    ADDON.setSetting('ga_time', str(now).split('.')[0])
+    APP_LAUNCH()    
+    
+                    
+def send_request_to_google_analytics(utm_url):
+    ua='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+    import urllib2
+    try:
+        req = urllib2.Request(utm_url, None,
+                                    {'User-Agent':ua}
+                                     )
+        response = urllib2.urlopen(req).read()
+    except:
+        print ("GA fail: %s" % utm_url)         
+    return response
+       
+def GA(group,name):
+        try:
+            try:
+                from hashlib import md5
+            except:
+                from md5 import md5
+            from random import randint
+            import time
+            from urllib import unquote, quote
+            from os import environ
+            from hashlib import sha1
+            VISITOR = ADDON.getSetting('visitor_ga')
+            utm_gif_location = "http://www.google-analytics.com/__utm.gif"
+            if not group=="None":
+                    utm_track = utm_gif_location + "?" + \
+                            "utmwv=" + VERSION + \
+                            "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                            "&utmt=" + "event" + \
+                            "&utme="+ quote("5("+PATH+"*"+group+"*"+name+")")+\
+                            "&utmp=" + quote(PATH) + \
+                            "&utmac=" + UATRACK + \
+                            "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR,VISITOR,"2"])
+                    try:
+                        print "============================ POSTING TRACK EVENT ============================"
+                        send_request_to_google_analytics(utm_track)
+                    except:
+                        print "============================  CANNOT POST TRACK EVENT ============================" 
+            if name=="None":
+                    utm_url = utm_gif_location + "?" + \
+                            "utmwv=" + VERSION + \
+                            "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                            "&utmp=" + quote(PATH) + \
+                            "&utmac=" + UATRACK + \
+                            "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR, VISITOR,"2"])
+            else:
+                if group=="None":
+                       utm_url = utm_gif_location + "?" + \
+                                "utmwv=" + VERSION + \
+                                "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                                "&utmp=" + quote(PATH+"/"+name) + \
+                                "&utmac=" + UATRACK + \
+                                "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR, VISITOR,"2"])
+                else:
+                       utm_url = utm_gif_location + "?" + \
+                                "utmwv=" + VERSION + \
+                                "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                                "&utmp=" + quote(PATH+"/"+group+"/"+name) + \
+                                "&utmac=" + UATRACK + \
+                                "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR, VISITOR,"2"])
+                                
+            print "============================ POSTING ANALYTICS ============================"
+            send_request_to_google_analytics(utm_url)
+            
+        except:
+            print "================  CANNOT POST TO ANALYTICS  ================" 
+            
+            
+def APP_LAUNCH():
+        versionNumber = int(xbmc.getInfoLabel("System.BuildVersion" )[0:2])
+        if versionNumber < 12:
+            if xbmc.getCondVisibility('system.platform.osx'):
+                if xbmc.getCondVisibility('system.platform.atv2'):
+                    log_path = '/var/mobile/Library/Preferences'
+                else:
+                    log_path = os.path.join(os.path.expanduser('~'), 'Library/Logs')
+            elif xbmc.getCondVisibility('system.platform.ios'):
+                log_path = '/var/mobile/Library/Preferences'
+            elif xbmc.getCondVisibility('system.platform.windows'):
+                log_path = xbmc.translatePath('special://home')
+                log = os.path.join(log_path, 'xbmc.log')
+                logfile = open(log, 'r').read()
+            elif xbmc.getCondVisibility('system.platform.linux'):
+                log_path = xbmc.translatePath('special://home/temp')
+            else:
+                log_path = xbmc.translatePath('special://logpath')
+            log = os.path.join(log_path, 'xbmc.log')
+            logfile = open(log, 'r').read()
+            match=re.compile('Starting XBMC \((.+?) Git:.+?Platform: (.+?)\. Built.+?').findall(logfile)
+        elif versionNumber > 11:
+            print '======================= more than ===================='
+            log_path = xbmc.translatePath('special://logpath')
+            log = os.path.join(log_path, 'xbmc.log')
+            logfile = open(log, 'r').read()
+            match=re.compile('Starting XBMC \((.+?) Git:.+?Platform: (.+?)\. Built.+?').findall(logfile)
+        else:
+            logfile='Starting XBMC (Unknown Git:.+?Platform: Unknown. Built.+?'
+            match=re.compile('Starting XBMC \((.+?) Git:.+?Platform: (.+?)\. Built.+?').findall(logfile)
+        print '==========================   '+PATH+' '+VERSION+'  =========================='
+        try:
+            from hashlib import md5
+        except:
+            from md5 import md5
+        from random import randint
+        import time
+        from urllib import unquote, quote
+        from os import environ
+        from hashlib import sha1
+        import platform
+        VISITOR = ADDON.getSetting('visitor_ga')
+        for build, PLATFORM in match:
+            if re.search('12',build[0:2],re.IGNORECASE): 
+                build="Frodo" 
+            if re.search('11',build[0:2],re.IGNORECASE): 
+                build="Eden" 
+            if re.search('13',build[0:2],re.IGNORECASE): 
+                build="Gotham" 
+            print build
+            print PLATFORM
+            utm_gif_location = "http://www.google-analytics.com/__utm.gif"
+            utm_track = utm_gif_location + "?" + \
+                    "utmwv=" + VERSION + \
+                    "&utmn=" + str(randint(0, 0x7fffffff)) + \
+                    "&utmt=" + "event" + \
+                    "&utme="+ quote("5(APP LAUNCH*"+build+"*"+PLATFORM+")")+\
+                    "&utmp=" + quote(PATH) + \
+                    "&utmac=" + UATRACK + \
+                    "&utmcc=__utma=%s" % ".".join(["1", VISITOR, VISITOR, VISITOR,VISITOR,"2"])
+            try:
+                print "============================ POSTING APP LAUNCH TRACK EVENT ============================"
+                send_request_to_google_analytics(utm_track)
+            except:
+                print "============================  CANNOT POST APP LAUNCH TRACK EVENT ============================" 
+checkGA()
 
 def get_params():
         param=[]
